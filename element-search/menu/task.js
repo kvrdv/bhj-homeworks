ab29349558969ebf56e434900740bcfa7de05c86
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-05-22 08:51:04 +0300

    second attempt

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,33 +1,21 @@
 
-// 1. Зарегистрируйте обработчики события click на элементах с классом menu__link
-// 2. Найдите меню рядом со ссылкой. Если оно есть, переключите у него класс menu_active
-// 3. Запрещайте переход по ссылке для тех, что имеют вложенное меню. 
-// Остальные пункты меню должны без помех переводить пользователя на соответствующие страницы:
-let menuLink = document.getElementsByClassName("menu__link");
-let menuSub = document.getElementsByClassName("menu_sub");
+// Берем все ссылки:
+let menuLink = document.querySelectorAll(".menu__link");
 
-menuLink[1].onclick = function() {
-  menuSub[0].className = "menu menu_sub menu_active";
-  menuSub[1].className = "menu menu_sub";
-  return false; 
-}
+// Функция, которая выполняется для каждой ссылки:
+menuLink.forEach(function (elementMenuLink) {
+  // Берем родительский элемент ссылки:
+  let menuItem = elementMenuLink.parentElement;
+  // У этого родительского элемента берем ближайший элемент-потомок с классом ".menu_sub", то есть выпадающее меню:
+  let menuSub = menuItem.querySelector(".menu_sub");
 
-menuLink[5].onclick = function() {
-  menuSub[0].className = "menu menu_sub";
-  menuSub[1].className = "menu menu_sub menu_active";
-  return false;
-}
-
-// 1. Одновременно не должно быть открыто более одного вложенного меню. Все остальные должны быть скрыты
-// 2. Напишите код для случая, когда на странице может быть более 1 навигационного меню:
-menuLink[10].onclick = function() {
-  menuSub[2].className = "menu menu_sub menu_active";
-  menuSub[3].className = "menu menu_sub";
-  return false; 
-}
-
-menuLink[14].onclick = function() {
-  menuSub[2].className = "menu menu_sub";
-  menuSub[3].className = "menu menu_sub menu_active";
-  return false; 
-}
+  // Обрабочтик события - клик по ссылке:
+  elementMenuLink.onclick = function () {
+    // Закрываем все открытые выпадающие меню:
+    document.querySelectorAll(".menu_active").forEach((menu_item) => menu_item.classList.remove('menu_active'));
+    // Если у ссылки есть выпадающее меню, то оно открывается
+    menuSub.classList.add('menu_active');
+    // Запрещаем переход по ссылке:
+    return false;
+  }
+});
