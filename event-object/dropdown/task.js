358d10b9fd8c1181a9a00d8f7b0e8c433d26387f
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-05-24 15:49:35 +0300

    first attempt

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,22 +1,21 @@
 let link = document.querySelectorAll(".dropdown__link");
+let button = document.querySelector(".dropdown__value");
+let dropDown = document.querySelector(".dropdown__list");
+
+button.onclick = function () {
+  if (dropDown.classList.contains("dropdown__list_active")) {
+    dropDown.classList.remove("dropdown__list_active");
+  } else {
+    dropDown.classList.add("dropdown__list_active");
+  }
+};
 
 link.forEach(function (elementItem) {
-  let button = document.querySelector(".dropdown__value");
-  let dropDown = document.querySelector(".dropdown__list");
-    
-  // Открытие/закрытие выпадающего меню по клику на кнопку:
-  button.onclick = function () {
-    if (dropDown.classList.contains("dropdown__list_active")) {
-      dropDown.classList.remove("dropdown__list_active");
-    } else {
-      dropDown.classList.add("dropdown__list_active");
-    }
-  };
-
-  // Смена значения по клику:
   elementItem.onclick = function () {
-    let textValue = document.querySelector(".dropdown__value").textContent;
-    textValue = elementItem.textContent;
+    for (let i = 0; i < link.length; i++) {
+      let textValue = document.querySelector(".dropdown__value").textContent;
+      textValue = elementItem[i].textContent;
+    }
     dropDown.classList.remove("dropdown__list_active");
     return false;
   };
