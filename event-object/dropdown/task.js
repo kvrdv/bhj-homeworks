1ea5a30acf866e7b0b326577ce4a7dc43577a727
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-05-26 13:52:53 +0300

    first attempt

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,22 +1,21 @@
-let link = document.querySelectorAll(".dropdown__link");
-let button = document.querySelector(".dropdown__value");
-let dropDown = document.querySelector(".dropdown__list");
+let button = document.querySelector('.dropdown__value');
+let link = document.querySelectorAll('.dropdown__link');
+let list = document.querySelector('.dropdown__list');
 
-button.onclick = function () {
-  if (dropDown.classList.contains("dropdown__list_active")) {
-    dropDown.classList.remove("dropdown__list_active");
-  } else {
-    dropDown.classList.add("dropdown__list_active");
-  }
-};
-
-link.forEach(function (elementItem) {
-  elementItem.onclick = function () {
-    for (let i = 0; i < link.length; i++) {
-      let textValue = document.querySelector(".dropdown__value").textContent;
-      textValue = elementItem[i].textContent;
-    }
-    dropDown.classList.remove("dropdown__list_active");
+button.addEventListener('click', function () {
+  if (list.classList.contains('dropdown__list_active')) {
+    list.classList.remove('dropdown__list_active');
     return false;
-  };
-}); 
+  } else {
+    list.classList.add('dropdown__list_active');
+  }
+});
+
+for (let i = 0; i < link.length; i++) {
+  link[i].addEventListener('click', function (e) {
+    e.preventDefault();
+    button.textContent = link[i].textContent;
+    list.classList.remove('dropdown__list_active');
+    return false;
+  })
+};
