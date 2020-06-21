3c7b859293ab4169356f6fe7d04e89d1350dac94
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-06-21 15:52:18 +0300

    second commit

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,28 +1,36 @@
+// Форма и ее значение:
+let form = document.querySelector('.tasks__control');
+let formInput = document.querySelector('.tasks__input')
 
-// Навешиваем обработчик события, нажатие на клавишу enter, на форму:
-document.querySelector('.tasks__input').addEventListener('keydown', function (event) { 
+// Cписок задач:
+let list = document.querySelector('.tasks__list');
 
-  // Получаем список задач:
-  let list = document.querySelector('.tasks__list');
+// Обработчик события - отправка формы:
+form.addEventListener('submit', function (e) {
 
-  // При нажатии на клавишу enter и при условии, что форма не пустая, добавляем задачу в конец списка:
-  if ((event.keyCode === 13) && (this.value !== '')) {
+  // Предотвращаем перезагрузку страницы:
+  e.preventDefault();
+
+  // Добавляем задачу если форма не пустая и делаем предупреждение, если пустая:
+  if (formInput.value !== '') {
     list.insertAdjacentHTML('beforeEnd', '<div class="task"><div class="task__title">Тут должен быть текст</div><a href="#" class="task__remove">&times;</a></div>');
     let task = document.querySelectorAll('.task__title');
 
-    // Поскольку задачу добавляем в конец списка, тут нам нужен последний эелемент массива:
-    task[task.length - 1].textContent = this.value;
+    // Поскольку задачу добавляем в конец списка, тут нам нужен последний элемент массива:
+    task[task.length - 1].textContent = formInput.value;
 
     // Получаем список закрывающих кнопок:
     let closeButtons = document.querySelectorAll('.task__remove');
 
     // Удаляем задачу, при нажатии на крестик:
-    closeButtons[closeButtons.length -1].addEventListener('click', (deleteTask) => {
+    closeButtons[closeButtons.length - 1].addEventListener('click', (deleteTask) => {
       deleteTask.currentTarget.parentElement.remove()
     });
 
-    // Очищаем форму и предотвращаем обновление страницы:
-    this.value = '';
-    event.preventDefault();
+    // Очищаем форму:
+    formInput.value = '';
+
+  } else if (formInput.value == '') {
+    alert('Невозможно добавить пустую задачу!');
   };
 });
